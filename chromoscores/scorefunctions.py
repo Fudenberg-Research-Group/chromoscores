3cd5b1f3483d3965dfd7431e8a5ed7c2f86c873f
Hadi <rahmanin@Hadis-MacBook-Pro.local>
2023-03-28 23:56:39 -0700

    working on numpy-style

--- chromoscores/scorefunctions.py
+++ chromoscores/scorefunctions.py
@@ -47,6 +47,22 @@ def tad_snippet(contact_map, stall_list, index):
     Tad snippet
     To extract Tad snippets adjacent to a boundary element with index of
     "index" from a list of "stall_list":
+    -------------------------
+    Function tad_snippet(contact_map, stall_list, index):
+
+         begin function
+
+         raise an error if index is out of list range
+
+         set tad_matrix between sequential stalls starting with index:
+             tad = contact_map[
+                   stall_list[index] : stall_list[index + 1] + 1,
+                   stall_list[index] : stall_list[index + 1] + 1,
+                   ]
+
+    return tad_matrix
+
+    end function
     """
 
     if index + 1 > len(stall_list):
@@ -59,16 +75,43 @@ def tad_snippet(contact_map, stall_list, index):
     return tad
 
 
-
-
 def tad_snippet_sectors(
     contact_map, stall_list, index, delta, diag_offset, max_distance
 ):
-    '''
+    """
     Tad snippet sectors
     setting area in_tad vs area out_tad. "Delta" is defined to exclude
     flames when extracting in_tad and out_tad areas.
-    '''
+    --------------------------------------
+    Function tad_snippet_sectors(contact_map, stall_list, index, delta, diag_offset, max_distance):
+
+         begin function
+
+         getting tad_matrix from tad_snippet function
+
+         set adjacent_tads starting with stall_index:
+             pile_center = contact_map[
+             stall_list[index] : stall_list[index + 2] + 1,
+             stall_list[index] : stall_list[index + 2] + 1,
+             ]
+
+         raise an error if max_distance is larger than snippet
+
+         set in_tad and out_tad areas:
+             out_tad = np.zeros(np.shape(pile_center))
+             out_tad[delta : tad_size - delta, tad_size + delta : -delta] = 1
+             out_tad = np.tril(np.triu(out_tad, diag_offset), max_distance) > 0
+
+             in_tad = np.zeros(np.shape(pile_center))
+             in_tad[delta : tad_size - delta, delta : tad_size - delta] = 1
+             in_tad[tad_size + delta : -delta, tad_size + delta : -delta] = 1
+             in_tad = np.tril(np.triu(in_tad, diag_offset), max_distance) > 0
+
+    return in_tad, out_tad, adjacent matrices
+
+    end function
+    --------------------------------------
+    """
     tad = tad_snippet(contact_map, stall_list, index)
     tad_size = len(tad)
 
@@ -92,15 +135,28 @@ def tad_snippet_sectors(
     return in_tad, out_tad, pile_center
 
 
-
-
 def flame_snippet_vertical(contact_map, stall_list, index, width, edge_length):
-    '''
+    """
     Flame snippet (vertical)
     To extract snippets around a vertical flame due to a boundary element
     with index "index" from a list of boundary elements "stall_list":
     ("edge_length" is the excluded areas at the ends of the flame)
-    '''
+    ------------------------------------------
+    Function flame_snippet_vertical(contact_map, stall_list, index, width, edge_length):
+
+         begin function
+
+         set snippet_matrix from contact_map containing flame with a selected width:
+             snippet = contact_map[
+             (stall_list[n] + edge_length) : (stall_list[n + 1] - edge_length),
+             (stall_list[n + 1] - width) : (stall_list[n + 1] + width),
+             ]
+
+    return snippet_matrix
+
+    end function
+    ------------------------------------------
+    """
     snippet = contact_map[
         (stall_list[n] + edge_length) : (stall_list[n + 1] - edge_length),
         (stall_list[n + 1] - width) : (stall_list[n + 1] + width),
@@ -108,16 +164,28 @@ def flame_snippet_vertical(contact_map, stall_list, index, width, edge_length):
     return snippet
 
 
-
-
-
 def flame_snippet_horizontal(contact_map, stall_list, index, size, edge_length):
-    '''
+    """
     Flame snippet (horizontal)
     To extract snippets of a horizontal flame due to a boundary element with
     index "index" from a list of boundary elements "stall_list": ("edge_length"
     is the excluded areas at the end of the flame)
-    '''
+    ---------------------------------------
+    Function flame_snippet_horizontal(contact_map, stall_list, index, width, edge_length):
+
+         begin function
+
+         set snippet_matrix from contact_map containing flame with a selected width:
+             snippet = contact_map[
+             (stall_list[n] - width) : (stall_list[n] + width),
+             (stall_list[n] + edge_length) : (stall_list[n + 1] - edge_length),
+             ]
+
+    return snippet_matrix
+
+    end function
+    ---------------------------------------
+    """
     snippet = contact_map[
         (stall_list[n] - width) : (stall_list[n] + width),
         (stall_list[n] + edge_length) : (stall_list[n + 1] - edge_length),
@@ -236,9 +304,9 @@ def tad_score(contact_map, stall_list, index, delta, diag_offset, max_distance):
 
 ######## Flame scores #########
 def flame_score_v(flame_snippet, flame_thickness, background_thickness):
-    '''
-    vertical flame score 
-    '''
+    """
+    vertical flame score
+    """
     mid = (np.shape(flame_snippet)[1]) // 2 + 1
     return np.mean(
         avg_peaks[:, mid - flame_thickness // 2 : mid + flame_thickness // 2]
@@ -247,11 +315,10 @@ def flame_score_v(flame_snippet, flame_thickness, background_thickness):
     )
 
 
-
 def flame_score_h(flame_snippet, flame_thickness, background_thickness):
-    '''
+    """
     horizontal flame score
-    '''
+    """
     mid = len(flame_snippet) // 2 + 1
     return np.mean(
         flame_snippet[mid - flame_thickness // 2 : mid + flame_thickness // 2, :]
