8480ed4de5353e21fe2530ee8355145a2734f676
Hadi <rahmanin@Hadis-MacBook-Pro.local>
2023-05-12 10:35:24 -0700

    adding more tests with nonzer0 pseudo_count

--- tests/test_scorefunctions.py
+++ tests/test_scorefunctions.py
@@ -2,17 +2,15 @@ from chromoscores.scorefunctions import *
 
 
 mat = [[1, 1, 2], [1, 4, 1], [2, 1, 1]]
-mat=np.array(mat)
-assert (
-    peak_score_upperRight(mat, peak_width=1, background_width=1, pseudo_count=0) == 2
-)
-assert (
-    peak_score_upperLeft(mat, peak_width=1, background_width=1, pseudo_count=0) == 4
-)
-assert (
-    peak_score_lowerRight(mat, peak_width=1, background_width=1, pseudo_count=0) == 4
-)
-assert (
-    peak_score_lowerLeft(mat, peak_width=1, background_width=1, pseudo_count=0) == 2
-)
+mat = np.array(mat)
+assert peak_score_upperRight(mat, peak_width=1, background_width=1, pseudo_count=0) == 2
+assert peak_score_upperLeft(mat, peak_width=1, background_width=1, pseudo_count=0) == 4
+assert peak_score_lowerRight(mat, peak_width=1, background_width=1, pseudo_count=0) == 4
+assert peak_score_lowerLeft(mat, peak_width=1, background_width=1, pseudo_count=0) == 2
 assert peak_score(mat, peak_width=1, background_width=1, pseudo_count=0) == 3
+
+
+########non-zero pseudo count
+mat = [[0, 0, 0], [0, 1, 0], [0, 0, 0]]
+mat = np.array(mat)
+assert peak_score(mat, peak_width=1, background_width=1, pseudo_count=1) == 2
