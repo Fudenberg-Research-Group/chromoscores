d9b9746d03be35a3c449f883cedb093c3fa54591
Hadi <rahmanin@Hadis-MBP.usc.edu>
2023-05-24 10:20:19 -0700

    adding tests for isolation scores

--- tests/test_scorefunctions.py
+++ tests/test_scorefunctions.py
@@ -14,3 +14,20 @@ assert peak_score(mat, peak_width=1, background_width=1, pseudo_count=0) == 3
 mat = [[0, 0, 0], [0, 1, 0], [0, 0, 0]]
 mat = np.array(mat)
 assert peak_score(mat, peak_width=1, background_width=1, pseudo_count=1) == 2
+
+
+""" isolation snippets/score """
+example_A=np.array(np.ones((18,18)))
+a,b,c=get_isolation_snippets(example_A,1,3,10,1)
+
+mat=a+b
+assert isolation_score(mat,delta=1,diag_offset=3,max_dist=10,state=1,pseudo_count=1)==1
+
+mat=3*a+b
+assert isolation_score(mat,delta=1,diag_offset=3,max_dist=10,state=1,pseudo_count=1)=2
+
+
+mat=a+3*b
+assert isolation_score(mat,delta=1,diag_offset=3,max_dist=10,state=1,pseudo_count=1)==0.5
+
+
