07a57e48116ab54b934dd783eaeebd395016c913
Hadi <rahmanin@Hadis-MBP.usc.edu>
2023-06-06 11:15:12 -0700

    resolving issues

--- chromoscores/scorefunctions.py
+++ chromoscores/scorefunctions.py
@@ -21,6 +21,9 @@ def peak_score_upperRight(
     ratio of the mean of the peak and the mean of the background
 
     """
+    if len(background_width) > len(peak_snippet // 2):
+        raise ValueError("background_width exceeds the size of the snippet")
+
     mid = len(peak_snippet) // 2
     peak_interior = pseudo_count + np.mean(
         peak_snippet[
@@ -54,6 +57,9 @@ def peak_score_lowerRight(
     ratio of the mean of the peak and the mean of the background
 
     """
+    if len(background_width) > len(peak_snippet // 2):
+        raise ValueError("background_width exceeds the size of the snippet")
+
     mid = len(peak_snippet) // 2
     peak_interior = pseudo_count + np.mean(
         peak_snippet[
@@ -87,6 +93,9 @@ def peak_score_upperLeft(
     ratio of the mean of the peak and the mean of the background
 
     """
+    if len(background_width) > len(peak_snippet // 2):
+        raise ValueError("background_width exceeds the size of the snippet")
+
     mid = len(peak_snippet) // 2
     peak_interior = pseudo_count + np.mean(
         peak_snippet[
@@ -120,6 +129,9 @@ def peak_score_lowerLeft(
     ratio of the mean of the peak and the mean of the background
 
     """
+    if len(background_width) > len(peak_snippet // 2):
+        raise ValueError("background_width exceeds the size of the snippet")
+
     mid = len(peak_snippet) // 2
     peak_interior = pseudo_count + np.mean(
         peak_snippet[
@@ -148,7 +160,7 @@ def peak_score(
     ----------
     peak_snippet: snippet of the contact map around the peak
     peak_width: width of the peak
-    background_width: width of the background outside the peak but inside the snippet 
+    background_width: width of the background outside the peak but inside the snippet
     pseudo_count: pseudo count to avoid division by zero
 
     returns
@@ -186,6 +198,61 @@ def peak_score(
 
 
 """Isolation score"""
+def _get_isolation_areas(
+    contact_map, delta=1, diag_offset=3, max_distance=10, state=1
+):
+    """
+    parameters
+    ----------
+    contact_map: snippet of a contact map around a boundary element
+    delta: distance from the border between in_tad and out_tad
+    diag_offset: distance from the diagonal. This also determines the size of the snippet.
+    max_distance: maximum distance from the diagonal
+    state: 1 for triangle snippets, 0 for square snippets
+
+    returns
+    -------
+    areas with a size of diag_offset inside and outside a tad
+    """
+    csize = len(contact_map) // 2
+    window_size = 4 * (diag_offset + delta) + 1
+    pile_center = contact_map[
+        csize - window_size // 2 : csize + window_size // 2 + 1,
+        csize - window_size // 2 : csize + window_size // 2 + 1,
+    ]
+
+    out_tad = np.zeros(np.shape(pile_center))
+    mask_out = np.zeros(np.shape(pile_center), dtype=bool)
+    mask_out[
+        window_size // 2 - diag_offset : window_size // 2,
+        window_size // 2 + 1 : window_size // 2 + diag_offset + 1,
+    ] = True
+    out_tad[mask_out] = pile_center[mask_out]
+    out_tad = np.tril(np.triu(out_tad, state * (diag_offset + 1)), max_distance)
+
+    in_tad = np.zeros(np.shape(pile_center))
+    mask = np.zeros(np.shape(pile_center), dtype=bool)
+    mask[
+        delta : delta + diag_offset,
+        diag_offset + delta + 1 : 2 * diag_offset + delta + 1,
+    ] = True
+    in_tad[mask] = pile_center[mask]
+
+    mask = np.zeros(np.shape(pile_center), dtype=bool)
+    mask[
+        window_size // 2 + delta : window_size // 2 + diag_offset + delta,
+        window_size // 2
+        + diag_offset
+        + delta
+        + 1 : window_size // 2
+        + 2 * diag_offset
+        + delta
+        + 1,
+    ] = True
+    in_tad[mask] = pile_center[mask]
+    in_tad = np.tril(np.triu(in_tad, state * (diag_offset + 1)), max_distance)
+
+    return in_tad, out_tad, pile_center
 
 
 def isolation_score(snippet, delta, diag_offset, max_dist, state, pseudo_count=1):
@@ -205,7 +272,7 @@ def isolation_score(snippet, delta, diag_offset, max_dist, state, pseudo_count=1
     ratio of the mean of the area inside tads and the area outside tad
 
     """
-    in_tad, out_tad, pile_center = get_isolation_snippets(
+    in_tad, out_tad, pile_center = _get_isolation_areas(
         snippet, delta, diag_offset, max_dist, state
     )
     assert pile_center.shape == (len(in_tad), len(in_tad))
@@ -216,33 +283,12 @@ def isolation_score(snippet, delta, diag_offset, max_dist, state, pseudo_count=1
 
 
 
-def tad_score(contact_map, boundary_list, index, delta, diag_offset, max_dist):
-    """
-    parameters
-    ----------
-    contact_map: contact map
-    boundary_list: list of boundary elements
-    delta: distance from the border between in_tad and out_tad. It is defined to exclude
-           flames when extracting in_tad and out_tad areas.
-    diag_offset: distance from the diagonal. This also determines the size of the snippet.
-    max_dist: maximum distance from the diagonal
-
-    returns
-    -------
-    ratio of the mean of the area inside tads and the area outside tad
-
-    """
-    in_tad, out_tad, pile_center = tad_snippet_sectors(
-        contact_map, boundary_list, index, delta, diag_offset, max_dist
-    )
-    assert pile_center.shape == (len(in_tad), len(in_tad))
-    return np.mean(pile_center[in_tad]) / np.mean(pile_center[out_tad])
-
-
 """Flame scores"""
 
 
-def flame_score_vertical(flame_snippet, flame_thickness, background_thickness, pseudo_count=1):
+def flame_score_vertical(
+    flame_snippet, flame_thickness, background_thickness, pseudo_count=1
+):
     """
     parameters
     ----------
@@ -256,10 +302,10 @@ def flame_score_vertical(flame_snippet, flame_thickness, background_thickness, p
     ratio of the mean of the flame and the mean of the background
     """
     mid = (np.shape(flame_snippet)[1]) // 2 + 1
-    flame_interior = pseudo_count+np.mean(
+    flame_interior = pseudo_count + np.mean(
         flame_snippet[:, mid - flame_thickness // 2 : mid + flame_thickness // 2]
     )
-    flame_background = pseudo_count+np.mean(
+    flame_background = pseudo_count + np.mean(
         flame_snippet[
             :, mid - background_thickness // 2 : mid + background_thickness // 2
         ]
@@ -268,7 +314,9 @@ def flame_score_vertical(flame_snippet, flame_thickness, background_thickness, p
     return flame_interior / flame_background
 
 
-def flame_score_horizontal(flame_snippet, flame_thickness, background_thickness, pseudo_count=1):
+def flame_score_horizontal(
+    flame_snippet, flame_thickness, background_thickness, pseudo_count=1
+):
     """
     parameters
     ----------
@@ -282,10 +330,10 @@ def flame_score_horizontal(flame_snippet, flame_thickness, background_thickness,
     ratio of the mean of the flame and the mean of the background
     """
     mid = len(flame_snippet) // 2 + 1
-    flame_interior = pseudo_count+np.mean(
+    flame_interior = pseudo_count + np.mean(
         flame_snippet[mid - flame_thickness // 2 : mid + flame_thickness // 2, :]
     )
-    flame_background = pseudo_count+np.mean(
+    flame_background = pseudo_count + np.mean(
         flame_snippet[
             mid - background_thickness // 2 : mid + background_thickness // 2, :
         ]
